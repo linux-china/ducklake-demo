e13cfbd4f7a1078efa0af09af91ce6aac4528425
Libing Chen <libing.chen@gmail.com>
2025-05-29 22:28:40 +0800

    test: demo.sql

---
+++ demo.sql
@@ -0,0 +1,7 @@
+create table city(id int, name varchar(100), country varchar(100));
+
+INSERT INTO city (id, name, country)
+VALUES
+    (1, 'Hangzhou', 'CN'),
+    (2, 'San Francisco', 'US'),
+    (3, 'Davao', 'PH');
